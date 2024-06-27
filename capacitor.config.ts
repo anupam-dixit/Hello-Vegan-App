7f9ea10feb1f4e46e5554cd18103075a30e6276c
anupam-dixit <69384581+anupam-dixit@users.noreply.github.com>
2024-06-27 09:37:09 +0530

    79cg78yed

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.hello.vegans',
+  appId: 'com.vegans.hello',
   appName: 'Hello Vegans',
   webDir: 'www',
   server: {
